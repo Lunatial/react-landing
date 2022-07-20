2bfd9ad0f964a515283e704d8d7599143973f0ca
Feleky <mirko.feleky@skylc.hu>
2022-07-20 17:00:57 +0200

    update: structure

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -14,12 +14,12 @@ const HeroSection = () => {
             className="bg-themeable-dark"
             style={{minHeight: 'calc(100vh - 44px)'}}>
             <div className="flex flex-col lg:flex-row gap-50 container mx-auto h-full items-center">
-                <div className="basis-1/2 flex-auto w-full">
+                <div className="basis-1/2 w-full">
                     <div className="max-w-xl">
                         <Image layout='responsive' height={600} width={600} src={heroImg} alt="Hero img"/>
                     </div>
                 </div>
-                <div className="basis-1/2 flex-auto">
+                <div className="basis-1/2">
                     <h1 className="text-6xl font-bold my-10 text-themeable-lightGray">
                         <FormattedMessage id="page.home.title"/>
                     </h1>
@@ -29,16 +29,13 @@ const HeroSection = () => {
                     <p className="text-left text-themeable-lightGray">
                         <ColoredText color="text-themeable-blue">„Lesznek még ennél többen is?”</ColoredText>
                         – kérdezte a tusványosszűz fotós kollégánk a bálványosi szabadegyetem nulladik napján
-                        a <ColoredText color="text-themeable-yellow">Tankcsapda-koncert</ColoredText> alatt, amire szinte egyszerre
+                        a <ColoredText color="text-themeable-yellow">Tankcsapda-koncert</ColoredText> alatt, amire
+                        szinte egyszerre
                         válaszoltuk, hogy igen, <ColoredText color="text-themeable-orange">Orbán Viktoron</ColoredText>.
                     </p>
                     <div className="flex flex-row justify-around mt-12">
-                        <div>
-                            <TryMe/>
-                        </div>
-                        <div>
-                            <AppLoader/>
-                        </div>
+                        <TryMe/>
+                        <AppLoader/>
                     </div>
                 </div>
             </div>
